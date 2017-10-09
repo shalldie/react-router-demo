08b49d5f63846418a92aefdeb986468649327afb
shalldie <394229785@qq.com>
2017-10-09 14:28:54 +0800

    拆分RouterHook到components

---
+++ src/components/RouterHook/index.js
@@ -0,0 +1,4 @@
+import RouterHook from './RouterHook';
+import { withRouter } from 'react-router-dom';
+
+export default withRouter(RouterHook);
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,47 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 // eslint-disable-next-line
 import { BrowserRouter as Router, Route, Link, Switch, withRouter } from 'react-router-dom';
 
-import CoreLayout from '../layout/CoreLayout';
+import CoreLayout from '../views/CoreLayout';
 import NavigatorView from './NavigatorView';
 import Home from './Home';
 import TodoList from './TodoList';
 import PageNotFound from './PageNotFound';
-import nprogress from 'nprogress';
 
-import 'nprogress/nprogress.css';
-
-
-/**
- * 路由钩子
- * 
- * @class RouterHook
- * @extends {Component}
- */
-class RouterHook extends Component {
-
-    constructor(props) {
-        super(props);
-
-        this.props.history.listen((location, action) => {
-            nprogress.start();
-            nprogress.done();
-        });
-    }
-
-    render() {
-        return (
-            this.props.children
-        );
-    }
-}
-
-let RouterHookView = withRouter(RouterHook);
+import RouterHook from '../components/RouterHook';
 
 const BasicRoutes = () => (
     <Router>
         {/* 给路由加一层hook */}
-        <RouterHookView>
+        <RouterHook>
             {/* 页面外部容器 */}
             <CoreLayout>
                 <Switch>
@@ -51,7 +23,7 @@ const BasicRoutes = () => (
                     <Route component={PageNotFound} />
                 </Switch>
             </CoreLayout>
-        </RouterHookView>
+        </RouterHook>
     </Router>
 );
 
